138b5a68f13b9906633699f2758ac15add5e3be8
hwd <347944701@qq.com>
2021-12-28 23:44:13 +0800

    create

---
+++ src/main/java/com/crush/workoncloud/WorkOnCloudApplication.java
@@ -0,0 +1,13 @@
+package com.crush.workoncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOnCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkOnCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crush/workoncloud/WorkOnCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crush.workoncloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOnCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
