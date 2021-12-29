1b5f3c6d2fc93e5577027fe4d4b7f213a10b9c84
hwd <347944701@qq.com>
2021-12-29 23:00:41 +0800

    验证码

---
+++ server/src/main/java/com/crush/server/controller/LoginController.java
@@ -0,0 +1,50 @@
+package com.crush.server.controller;
+
+import com.crush.server.pojo.Admin;
+import com.crush.server.pojo.AdminLoginParam;
+import com.crush.server.pojo.RespBean;
+import com.crush.server.service.IAdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+/**
+ * 登录
+ */
+@Api(tags = "LoginController")
+@RestController
+public class LoginController {
+    @Autowired
+    private IAdminService adminService;
+
+    @ApiOperation(value = "登录之后返回token")
+    @PostMapping("/login")
+    public RespBean login(@RequestBody AdminLoginParam adminLoginParam, HttpServletRequest request){
+        return adminService.login(adminLoginParam.getUsername(),adminLoginParam.getPassword(),adminLoginParam.getCode(),request);
+    }
+
+    @ApiOperation(value = "退出登录")
+    @PostMapping("/logout")
+    public RespBean logout(){
+        return RespBean.success("注销成功");
+    }
+
+    @ApiOperation(value = "获取当前登录信息")
+    @GetMapping("/admin/info")
+    public Admin getAdminInfo(Principal principal){
+        if (null == principal){
+            return null;
+        }
+        String username = principal.getName();
+        Admin admin = adminService.getAdminByUserName(username);
+        admin.setPassword(null);
+        return admin;
+    }
+}
--- server/src/main/java/com/crush/server/service/IAdminService.java
+++ server/src/main/java/com/crush/server/service/IAdminService.java
@@ -2,6 +2,9 @@ package com.crush.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.crush.server.pojo.Admin;
+import com.crush.server.pojo.RespBean;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -12,5 +15,20 @@ import com.crush.server.pojo.Admin;
  * @since 2021-12-29
  */
 public interface IAdminService extends IService<Admin> {
+    /**
+     * 登录之后返回token
+     * @param username
+     * @param password
+     * @param code
+     * @param request
+     * @return
+     */
+    RespBean login(String username, String password, String code, HttpServletRequest request);
 
+    /**
+     * 根据用户名获得用户
+     * @param username
+     * @return
+     */
+    Admin getAdminByUserName(String username);
 }
